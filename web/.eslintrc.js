fc88ffa247da7d7ac1851c771c72429123bcfa6a
Solidverse <t.amatk@protonmail.com>
2023-02-25 11:32:41 +0100

    FWR-2 Create scaffolding for schema web app

---
+++ web/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    root: true,
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+    },
+    env: {
+        node: true
+    },
+    ignorePatterns: ['.eslintrc.js'],
+    extends: [
+        // 'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:vue/vue3-recommended',
+        '@vue/typescript/recommended',
+        // 'prettier'
+        'plugin:prettier/recommended'
+    ],
+    rules: {
+        'quotes': ['error', 'single'],
+        'no-undef': 'off',
+        'vue/multi-word-component-names': 'off',
+        'vue/component-name-in-template-casing': ['error', 'PascalCase']
+    }
+};
