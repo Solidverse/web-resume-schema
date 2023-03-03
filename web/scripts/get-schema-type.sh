ab7b949dcaa186b971bd8857c2d050299d5378fd
Solidverse <t.amatk@protonmail.com>
2023-03-03 12:18:34 +0100

    FWR-2 Add schema type gen script

---
+++ web/scripts/get-schema-type.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Create a temporary directory to download the JSON schema file
+mkdir -p schematemp
+
+# Download the JSON schema file into the temp directory
+wget https://raw.githubusercontent.com/Solidverse/web-resume-schema/main/schema.json -O schematemp/schema.json
+
+# Generate TypeScript types from the schema
+npx --debug json-schema-to-typescript -i schematemp/schema.json -o types/schema.d.ts
+
+# Cleanup the temporary directory
+rm -rf schematemp
