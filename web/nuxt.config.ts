3f80876166a9a5b49d0e424f9ed1b10403b6897f
Solidverse <t.amatk@protonmail.com>
2023-03-08 13:59:12 +0100

    FWR-2 Add nuxt-icon & iconify recommendation

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
+  modules: [
+    '@pinia/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode',
+    'nuxt-icon',
+  ],
   colorMode: {
     preference: 'system', // default theme
     dataValue: 'theme', // activate data-theme in <html> tag
