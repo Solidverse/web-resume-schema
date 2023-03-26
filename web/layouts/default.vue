ba70d0c0abdeeb3b21e27545909f72492540b15b
Solidverse <t.amatk@protonmail.com>
2023-03-26 12:40:10 +0200

    FWR-2 Add prose to layout

--- web/layouts/default.vue
+++ web/layouts/default.vue
@@ -6,7 +6,7 @@
 
     <main>
       <div
-        class="container mx-auto px-5 md:max-w-screen-lg mt-20 md:mt-36 pb-10"
+        class="container mx-auto px-5 md:max-w-screen-lg mt-20 md:mt-36 pb-10 prose"
       >
         <slot />
       </div>
